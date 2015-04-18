5727ba5f295977792a7270be80b636a190e9fe15
300219631 <300219631@R6105S36.ad.douglascollege.ca>
2015-04-18 13:44:58 -0700

    Getting connection

---
+++ DesktopController_Client/src/com/example/desktopcontroller_client/Client.java
@@ -0,0 +1,41 @@
+package com.example.desktopcontroller_client;
+
+import java.net.Socket;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class Client extends Activity {
+	private String hostAddress;
+	private int port;
+	Socket client;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.client_layout);
+		
+		try{
+			
+			Intent data = getIntent();
+			hostAddress = data.getStringExtra("host");
+			port = data.getIntExtra("port", 0 );
+			
+			if(port > 0){
+				client = new Socket(hostAddress,port);
+				String msg = "Connected to: "+client.getInetAddress().getHostAddress();
+				Toast.makeText(this, msg ,Toast.LENGTH_LONG).show();
+				client.close();
+			}
+			else
+				Toast.makeText(this, "Invalid port Number",Toast.LENGTH_LONG).show();
+			
+		}catch(Exception ex){
+			Toast.makeText(this, "Error: "+ex.getMessage(),Toast.LENGTH_LONG).show();
+		}
+		
+	}
+
+}
--- DesktopController_Client/src/com/example/desktopcontroller_client/MainActivity.java
+++ DesktopController_Client/src/com/example/desktopcontroller_client/MainActivity.java
@@ -1,34 +1,40 @@
 package com.example.desktopcontroller_client;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
-
+	private EditText txtHost, txtPort;
+	private Button btnConnect;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		txtHost = (EditText) findViewById(R.id.txtHost);
+		txtPort = (EditText) findViewById(R.id.txtPort);
+		btnConnect = (Button) findViewById(R.id.btnConnect);
+		
+		btnConnect.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				String host = txtHost.getText().toString();
+				String port = txtPort.getText().toString();
+				
+				Intent intent = new Intent(getApplicationContext(), Client.class);
+				intent.putExtra("host",host);
+				intent.putExtra("",host);
+				startActivity(intent);
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
